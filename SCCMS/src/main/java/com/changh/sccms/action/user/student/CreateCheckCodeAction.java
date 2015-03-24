ff51e43b50a1e58cf892dd75a5bdc743660aeded
vigo2013 <fw121fw4@163.com>
2015-03-24 10:46:34 +0800

    修改验证码类

--- SCCMS/src/main/java/com/changh/sccms/action/user/student/CreateCheckCodeAction.java
+++ SCCMS/src/main/java/com/changh/sccms/action/user/student/CreateCheckCodeAction.java
@@ -6,10 +6,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+
 import com.changh.sccms.action.BaseAction;
 import com.changh.sccms.until.ImageUtil;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class CreateCheckCodeAction extends BaseAction{
 	private InputStream input;
@@ -28,16 +28,11 @@ public class CreateCheckCodeAction extends BaseAction{
 	{
 		Map<String,BufferedImage> map = ImageUtil.getImage();
 		String code = map.keySet().iterator().next();
-		//锟斤拷取锟斤拷证锟斤拷图片锟斤拷映锟斤拷
 		BufferedImage image = map.get(code);
-		//压锟斤拷锟斤拷jpeg锟斤拷式锟斤拷outputStream
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		JPEGImageEncoder encoder =JPEGCodec.createJPEGEncoder(bos);
-		encoder.encode(image);
-		//锟斤拷图片锟斤拷锟街斤拷锟斤拷锟斤拷,锟斤拷锟斤拷inputStream锟斤拷
+		ImageIO.write(image,"JPEG",bos);
 		byte[] bytes=bos.toByteArray();
 		input = new ByteArrayInputStream(bytes);
-		//锟斤拷code值锟斤拷session
 		session.put("code", code);
 		return "success";
 	}
